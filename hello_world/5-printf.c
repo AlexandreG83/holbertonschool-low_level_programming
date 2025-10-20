043b45147d6d0c407b80911861ab0007e64963b5
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-10-20 14:20:07 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints a sentence with proper grammar
+ *
+ * Description: This function prints the sentence
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
