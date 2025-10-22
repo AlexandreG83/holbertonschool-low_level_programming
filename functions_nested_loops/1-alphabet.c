4ba4c5eb0a37c972bee8350ec81ef54fdce4cc7b
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-10-22 12:25:09 +0000

    alphabet avec main.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints the last digit of a random number
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int lettre;
+
+/* your code goes there */
+for (lettre = 'a'; lettre <= 'z' ; lettre++)
+	{
+	_putchar(lettre);
+	}
+_putchar('\n');
+}
