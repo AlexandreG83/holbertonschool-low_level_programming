57cc77c85accbf106e646c5a21e09d95539e0bd1
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-10-22 08:22:13 +0000

    tabulations

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of a random number and its description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int lettre;
+
+/* Your code goes there */
+for (lettre = 0; lettre < 16; lettre++)
+{
+if (lettre < 10)
+	putchar(lettre + '0');
+else
+	putchar('a' + (lettre - 10));
+}
+putchar('\n');
+return (0);
+}
