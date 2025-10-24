9d5e6a2a037e7b76f9c4ada2f00b2b74db3191e9
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-10-24 08:25:16 +0000

    printf par _putchar

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * jack_bauer - prints every minute of the day from 00:00 to 23:59
@@ -12,7 +12,12 @@ for (hour = 0; hour < 24; hour++)
 {
 	for (minute = 0; minute < 60; minute++)
 	{
-		printf("%02d:%02d\n", hour, minute);
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((minute / 10) + '0');
+		_putchar((minute % 10) + '0');
+		_putchar('\n');
 	}
 }
 }
