c47e6f0ddb6ee1366b3acda9d77dd6faa63230fc
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-10-24 12:36:01 +0000

    write, 2 partie pour les 80 caracteres

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,7 +8,10 @@
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 59);
+char part1[] = "and that piece of art is useful\"";
+char part2[] = " - Dora Korpar, 2015-10-19\n";
+
+write(STDERR_FILENO, part1, sizeof(part1) - 1);
+write(STDERR_FILENO, part2, sizeof(part2) - 1);
 return (1);
 }
