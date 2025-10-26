726d4fce3a91bd2a501580035c954084e7512979
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-10-26 15:14:39 +0000

    4 lignes de moins, mais 2 lignes a retirer

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,53 +2,48 @@
 
 /**
  * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
- *
  * Return: Always 0.
  */
 int main(void)
 {
 int count;
-unsigned long a = 1, b = 2, a_half1, a_half2, b_half1, b_half2;
-unsigned long half1, half2;
-unsigned long limit = 1000000000; /* 1 billion to split numbers */
+unsigned long a = 1, b = 2, next;
+unsigned long a1, a2, b1, b2, half1, half2;
+unsigned long limit = 1000000000;
 
 printf("%lu, %lu", a, b);
 
 for (count = 3; count <= 98; count++)
 {
-	if (a + b < limit)
+	if (b < 1000000000000000000)
 	{
-		unsigned long next = a + b;
-
+		next = a + b;
 		printf(", %lu", next);
 		a = b;
 		b = next;
 	}
 	else
 	{
-		if (count == 3)
+		a1 = a / limit;
+		a2 = a % limit;
+		b1 = b / limit;
+		b2 = b % limit;
+
+		for (; count <= 98; count++)
 		{
-			a_half1 = a / limit;
-			a_half2 = a % limit;
-			b_half1 = b / limit;
-			b_half2 = b % limit;
+			half1 = a1 + b1;
+			half2 = a2 + b2;
+			if (half2 >= limit)
+			{
+				half1 += 1;
+				half2 -= limit;
+			}
+			printf(", %lu%09lu", half1, half2);
+			a1 = b1;
+			a2 = b2;
+			b1 = half1;
+			b2 = half2;
 		}
-
-		half1 = a_half1 + b_half1;
-		half2 = a_half2 + b_half2;
-
-		if (half2 >= limit)
-		{
-			half1 += 1;
-			half2 -= limit;
-		}
-
-		printf(", %lu%09lu", half1, half2);
-
-		a_half1 = b_half1;
-		a_half2 = b_half2;
-		b_half1 = half1;
-		b_half2 = half2;
 	}
 }
 printf("\n");
