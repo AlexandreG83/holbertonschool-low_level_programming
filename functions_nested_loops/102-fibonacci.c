9cf6e5c14ac1a01014add6cea54e8c4dbc535584
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-10-26 14:29:26 +0000

    if != 100 -> ,

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+long int i, n1 = 1, n2 = 2, next;
+
+for (i = 1; i <= 100; i++)
+{
+	printf("%ld", n1);
+	if (i != 100)
+	{
+		printf(", ");
+	}
+	next = n1 + n2;
+	n1 = n2;
+	n2 = next;
+	}
+printf("\n");
+return (0);
+}
