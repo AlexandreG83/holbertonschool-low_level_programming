5d004617f1d687fd5ec0197a7f8d47150b09c3a8
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-10-31 19:01:05 +0000

    sans les tabulations

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,34 @@
  */
 int _atoi(char *s)
 {
-int i = 0;
-int sign = 1;
-int started = 0;
-int num = 0;
+	int i = 0;
+	int sign = 1;
+	unsigned int num = 0;
+	int started = 0;
 
-while (s[i] != '\0')
-{
-	if (s[i] >= '0' && s[i] <= '9')
+	/* scan characters */
+	while (s[i] != '\0')
 	{
-		started = 1;
-		num = num * 10 + (s[i] - '0');
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			num = num * 10 + (unsigned int)(s[i] - '0');
+		}
+		else
+		{
+			if (started)
+				break;
+			if (s[i] == '-')
+				sign = -sign;
+			/* '+' or any other char before digits is ignored except '-' toggles sign */
+		}
+		i++;
 	}
-	else
-	{
-		if (started)
-			break;
-		if (s[i] == '-')
-			sign = -sign;
-	}
-	i++;
+
+	/* convert unsigned accumulation to signed int result safely */
+	if (sign > 0)
+		return ((int) num);
+else
+	return ((int)(0u - num));
 }
-return (num * sign);
-}
+
