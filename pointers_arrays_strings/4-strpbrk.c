298a00d9171c04ba46e727cde62eeb6e530c9e3d
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-11-03 11:28:54 +0000

    i = 0;     while (s[i] != '\0')     {         j = 0;         while (accept[j] != '\0')

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer to the string to search
+ * @accept: pointer to the string containing bytes to match
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i, j;
+
+i = 0;
+while (s[i] != '\0')
+{
+	j = 0;
+	while (accept[j] != '\0')
+	{
+		if (s[i] == accept[j])
+		{
+			return (s + i);
+		}
+		j++;
+	}
+	i++;
+}
+return (0);
+}
