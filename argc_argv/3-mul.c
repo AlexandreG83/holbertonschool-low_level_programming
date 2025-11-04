1ccfd00ea1a09670632aaeed7f301727661ea40e
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-11-04 13:32:21 +0000

    atoi convertit une chaine en entier, eclusion de tous les test or 2 nombres

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, resultat;
+
+if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
+/*atoi convertit une chaine en entier*/
+num1 = atoi(argv[1]); 
+num2 = atoi(argv[2]);
+resultat = num1 * num2;
+printf("%d\n", resultat);
+return (0);
+}
