eba69044b819a2a72a054113b6d5f3fcf07b8de4
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-11-14 09:55:54 +0000

    l'execution de fonctionne pas?

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -2,16 +2,55 @@
 #include "main.h"
 
 /**
- * strtow - splits a string into words
- * @str: string to split
+ * word_count - counts the number of words in a string
+ * @str: the string to analyze
  *
- * Return: pointer to an array of strings (words) or NULL
+ * Return: number of words
+ */
+int word_count(char *str)
+{
+int count = 0, i = 0;
+
+while (str[i])
+{
+	while (str[i] == ' ')
+		i++;
+	if (str[i] != '\0')
+	{
+		count++;
+		while (str[i] != ' ' && str[i] != '\0')
+			i++;
+	}
+}
+return (count);
+}
+
+/**
+ * word_len - returns length of next word
+ * @str: string
+ *
+ * Return: length
+ */
+int word_len(char *str)
+{
+int len = 0;
+
+while (str[len] && str[len] != ' ')
+	len++;
+return (len);
+}
+
+ /**
+ * strtow - splits string into words
+ * @str: input string
+ *
+ * Return: pointer to an array of strings
  */
 char **strtow(char *str)
 {
 char **words;
-int i = 0, j = 0, k = 0, len = 0, wc;
- 
+int i = 0, j = 0, k = 0, n = 0, len = 0, wc;
+
 if (str == NULL || *str == '\0')
 	return (NULL);
 
@@ -23,7 +62,31 @@ words = malloc(sizeof(char *) * (wc + 1));
 if (words == NULL)
 	return (NULL);
 
-
-
+while (*str)
+{
+	while (*str == ' ')
+		str++;
+	if (*str)
+	{
+		len = word_len(str);
+		words[k] = malloc(len + 1);
+		if (!words[k])
+		{
+			while (k > 0)
+			{
+				free(words[k]);
+				k--;
+			}
+			free(words);
+			return (NULL);
+		}
+		for (n = 0; n < len; n++)
+			words[k][n] = str[i + n];
+		words[k][len] = '\0';
+		k++;
+		i = j;
+	}
+}
+words[k] = NULL;
 return (words);
 }
