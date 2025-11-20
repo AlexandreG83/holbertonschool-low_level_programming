c78922f2b1be9ff79fecd0ea53b6c06aa3c8f82c
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-11-20 09:27:19 +0000

    case 's': -> str = va_arg(args, char *); if (no str) -> str = (nil); betty 80 lines; char separator

--- variadic_functions/3-main.c
+++ variadic_functions/3-main.c
@@ -1,39 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "3-calc.h"
+#include "variadic_functions.h"
 
 /**
- * main - performs simple operations
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - check the code
  *
- * Return: 0 on success, or exits with error codes
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-int num1, num2;
-int (*op_func)(int, int);
-
-if (argc != 4)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-op_func = get_op_func(argv[2]);
-if (op_func == NULL || argv[2][1] != '\0')
-{
-	printf("Error\n");
-	exit(99);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-
-if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-{
-	printf("Error\n");
-	exit(100);
-}
-printf("%d\n", op_func(num1, num2));
-return (0);
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
 }
