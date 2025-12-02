7b19b2f27a253b4c53f55be31c41eef3475adcfb
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-02 10:55:59 +0000

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,38 +14,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_descriptor;
-    ssize_t read_octets, octets_written;
-    char *buffer;
+int file_descriptor;
+ssize_t read_octets, octets_written;
+char *buffer;
 
-    if (!filename)
-        return (0);
+if (!filename)
+	return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+file_descriptor = open(filename, O_RDONLY);
+if (file_descriptor == -1)
+	return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(file_descriptor);
-        return (0);
-    }
+buffer = malloc(sizeof(char) * letters);
+if (!buffer)
+{
+	close(file_descriptor);
+	return (0);
+}
 
-    read_octets = read(file_descriptor, buffer, letters);
-    if (read_octets == -1)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+read_octets = read(file_descriptor, buffer, letters);
+if (read_octets == -1)
+{
+	free(buffer);
+	close(file_descriptor);
+	return (0);
+}
 
-    octets_written = write(STDOUT_FILENO, buffer, read_octets);
-    free(buffer);
-    close(file_descriptor);
+octets_written = write(STDOUT_FILENO, buffer, read_octets);
+free(buffer);
+close(file_descriptor);
 
-    if (octets_written != read_octets)
-        return (0);
+if (octets_written != read_octets)
+	return (0);
 
-    return (octets_written);
+return (octets_written);
 }
