c11d8deaab3a2f6d7aaa1c39fdf0ea21acd9559e
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-03 13:20:27 +0000

    add def structs hash_table_s and hash_node_s

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,7 +1,39 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-hash_table_t *hash_table_create(unsigned long int size);
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key, string
+ * @value: The value corresponding to a key
+ * @next: Pointer to the next node
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ * @size: Size of the array
+ * @array: Array of size @size
+ *         Each cell points to the first node of a linked list
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
 unsigned long int hash_djb2(const unsigned char *str);
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int key_index(const unsigned char *key,
+	unsigned long int size);
 
 #endif
