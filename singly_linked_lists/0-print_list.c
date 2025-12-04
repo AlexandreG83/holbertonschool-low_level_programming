1b9e1315acc5a6c9c2ab02d1775394b9fb44ae0b
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-04 09:53:11 +0000

    %u au lieu de %lu

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ while (h != NULL)
 	if (h->str == NULL)
 		printf("[0] (nil)\n");
 	else
-		printf("[%lu] %s\n", h->len, h->str);
+		printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
 	count++;
 }
