d6f0bcaba89536750f5761cbd013f740045e2796
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-04 14:07:16 +0000

    0

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,8 +13,8 @@
  */
 void error_exit(int code, const char *msg, const char *arg)
 {
-    dprintf(STDERR_FILENO, msg, arg);
-    exit(code);
+	dprintf(STDERR_FILENO, msg, arg);
+	exit(code);
 }
 
 /**
@@ -23,11 +23,11 @@ void error_exit(int code, const char *msg, const char *arg)
  */
 void safe_close(int fd)
 {
-    if (close(fd) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-        exit(100);
-    }
+if (close(fd) == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
 }
 
 /**
@@ -39,47 +39,43 @@ void safe_close(int fd)
  */
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to;
-    ssize_t bytes_read, bytes_written;
-    char buffer[BUF_SIZE];
+int fd_from, fd_to;
+ssize_t bytes_read, bytes_written;
+char buffer[BUF_SIZE];
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+if (argc != 3)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+	error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        safe_close(fd_from);
-        error_exit(99, "Error: Can't write to %s\n", argv[2]);
-    }
-
-    while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
-    {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written != bytes_read)
-        {
-            safe_close(fd_from);
-            safe_close(fd_to);
-            error_exit(99, "Error: Can't write to %s\n", argv[2]);
-        }
-    }
-
-    if (bytes_read == -1)
-    {
-        safe_close(fd_from);
-        safe_close(fd_to);
-        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-    }
-
-    safe_close(fd_from);
-    safe_close(fd_to);
-
-    return (0);
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+	safe_close(fd_from);
+	error_exit(99, "Error: Can't write to %s\n", argv[2]);
+}
+while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+{
+	bytes_written = write(fd_to, buffer, bytes_read);
+	if (bytes_written != bytes_read)
+	{
+		safe_close(fd_from);
+		safe_close(fd_to);
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
+}
+if (bytes_read == -1)
+{
+	safe_close(fd_from);
+	safe_close(fd_to);
+	error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+}
+safe_close(fd_from);
+safe_close(fd_to);
+return (0);
 }
