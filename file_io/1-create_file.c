97bfe8a59bf0696da84e8ec28444c019fb992d38
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-04 13:32:31 +0000

    if (len > 0) {         w = write(file_descriptor, text_content, len);         if (w == -1 || (size_t)w != len)         {                 close(file_descriptor);                 return (-1);         } }

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,24 +5,21 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters to read and print
+ * create_file - creates a file and writes text_content to it
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write into the file
  *
- * Return: actual number of letters read and printed,
- *         0 on failure (cannot open/read/write or filename is NULL)
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-int file_descriptor;
-ssize_t octets_written;
+int file_descriptor, w;
 size_t len = 0;
 
 if (!filename)
 	return (-1);
 
-file_descriptor = open(filename, O_WRONLY | O_CREAT
-		       | O_TRUNC, S_IRUSR | S_IWUSR);
+file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (file_descriptor == -1)
 	return (-1);
 
@@ -30,8 +27,11 @@ if (text_content)
 {
 	while (text_content[len])
 		len++;
-	octets_written = write(file_descriptor, text_content, len);
-	if (octets_written != (ssize_t)len)
+}
+if (len > 0)
+{
+	w = write(file_descriptor, text_content, len);
+	if (w == -1 || (size_t)w != len)
 	{
 		close(file_descriptor);
 		return (-1);
@@ -40,3 +40,4 @@ if (text_content)
 close(file_descriptor);
 return (1);
 }
+
