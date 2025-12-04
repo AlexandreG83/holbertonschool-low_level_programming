adc831e8f15569b884cfab526911b74050a7cd41
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-04 14:18:41 +0000

    betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,24 +9,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int i;
+hash_table_t *ht;
+unsigned long int i;
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
+ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
+	return (NULL);
 
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return (NULL);
-    }
+ht->array = malloc(sizeof(hash_node_t *) * size);
+if (ht->array == NULL)
+{
+	free(ht);
+	return (NULL);
+}
 
-    for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
-
-    ht->size = size;
-
-    return (ht);
+for (i = 0; i < size; i++)
+	t->array[i] = NULL;
+ht->size = size;
+return (ht);
 }
