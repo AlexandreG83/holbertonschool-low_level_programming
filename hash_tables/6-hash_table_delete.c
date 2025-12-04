361ecceadac7b385fc70867584efad90a3d8eae5
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-04 14:37:33 +0000

    test

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,8 +1,12 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table
- * @ht: hash table
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to the hash table to delete
+ *
+ * Description: Frees all memory used by the hash table, including
+ * all nodes and their keys and values.
  */
 void hash_table_delete(hash_table_t *ht)
 {
@@ -24,6 +28,7 @@ for (i = 0; i < ht->size; i++)
 		node = tmp;
 	}
 }
+
 free(ht->array);
 free(ht);
 }
